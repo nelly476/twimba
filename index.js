c386e56aeada55eae14960f99dafefecd805b2fe
nelly476 <nellybulatova12@gmail.com>
2022-10-16 11:06:30 +0300

    add click handlers

--- index.js
+++ index.js
@@ -1,58 +1,119 @@
-const tweetsData = [   
-    {
-        handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
-        likes: 27,
-        retweets: 10,
-        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
-            Guaranteed return on investment. HMU DMs open!!`,
-        replies: [],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
-    },    
-    {
-        handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
-        likes: 6500,
-        retweets: 234,
-        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
-        replies: [
-                  {
-                handle: `@TomCruise ✅`,
-                profilePic: `images/tcruise.png`,
-                tweetText: `Yes! Sign me up! 😎🛩`,
-            },
-                  {
-                handle: `@ChuckNorris ✅`,
-                profilePic: `images/chucknorris.jpeg`,
-                tweetText: `I went last year😴`,
-            },
-        ],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
-    },
-        {
-        handle: `@NoobCoder12`,
-        profilePic: `images/flower.png`,
-        likes: 10,
-        retweets: 3,
-        tweetText: `Are you a coder if you only know HTML?`,
-        replies: [
-            {
-                handle: `@StackOverflower ☣️`,
-                profilePic: `images/overflow.png`,
-                tweetText: `No. Onviosuly not. Go get a job in McDonald's.`,
-            },
-            {
-                handle: `@YummyCoder64`,
-                profilePic: `images/love.png`,
-                tweetText: `You are wonderful just as you are! ❤️`,
-            },
-        ],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
-    },     
-]
+import { tweetsData } from "/data.js";
+const tweetInput = document.getElementById("tweet-input");
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+  }
+});
+
+function handleLikeClick(tweetId) {
+  const targetTweetObject = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+  if (!targetTweetObject.isLiked) {
+    targetTweetObject.likes++;
+  } else {
+    targetTweetObject.likes--;
+  }
+
+  targetTweetObject.isLiked = !targetTweetObject.isLiked;
+  render();
+}
+
+function handleRetweetClick(tweetId) {
+  const targetTweetObject = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (!targetTweetObject.isRetweeted) {
+    targetTweetObject.retweets++;
+  } else {
+    targetTweetObject.retweets--;
+  }
+
+  targetTweetObject.isRetweeted = !targetTweetObject.isRetweeted;
+  render();
+}
+
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
+}
+
+function handleTweetBtnClick() {
+  console.log(tweetInput.value);
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  tweetsData.forEach(function (tweet) {
+    let likeIconClass = "";
+    let retweetIconClass = "";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    } else if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
+    }
+
+    let repliesHtml = "";
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
+        <div class="tweet-reply">
+        <div class="tweet-inner">
+            <img src="${reply.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${reply.handle}</p>
+                    <p class="tweet-text">${reply.tweetText}</p>
+                </div>
+            </div>
+    </div>`;
+      });
+    }
+
+    feedHtml += `
+    <div class="tweet">
+<div class="tweet-inner">
+    <img src="${tweet.profilePic}" class="profile-pic">
+    <div>
+        <p class="handle">${tweet.handle}</p>
+        <p class="tweet-text">${tweet.tweetText}</p>
+        <div class="tweet-details">
+            <span class="tweet-detail">
+            <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
+                ${tweet.replies.length}
+            </span>
+            <span class="tweet-detail">
+            <i class="fa-solid fa-heart ${likeIconClass}" data-like="${tweet.uuid}"></i>
+            ${tweet.likes}
+            </span>
+            <span class="tweet-detail">
+            <i class="fa-solid fa-retweet ${retweetIconClass}" data-retweet="${tweet.uuid}"></i>
+            ${tweet.retweets}
+            </span> 
+        </div>   
+    </div>             
+</div>
+<div class="hidden" id="replies-${tweet.uuid}">
+${repliesHtml}
+</div>  
+</div>`;
+  });
+
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+}
+
+render();
